619cc482c30adbdd4c50598f7c37b407b2c6a36f
Benson <48788513+benson00077@users.noreply.github.com>
2022-07-17 17:27:52 +0800

    Feat emit and receive events
    Emit event when create posts and comments
    Receive event when each other service create posts and comments

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
-const cors = require('cors')
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -21,7 +22,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentID = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -31,7 +32,22 @@ app.post("/posts/:id/comments", (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentID,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Event Received", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
